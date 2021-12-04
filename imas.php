fd8a99e3786ce854e9c850f3fa0b3df1db605038
caffeine <caffeine.konchan@gmail.com>
2021-12-04 21:14:49 +0900

    first commit

---
+++ imas.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<meta charset="utf-8">
+<title>ミリシタ 宣材資料メーカー | アイドルマスター ミリオンライブ！ シアターデイズ</title>
+<meta name="description" content="アイドルたちの魅力をプロデューサーさんの力で伝えましょう！おすすめの楽曲、アピールポイントなどプロデューサーさん視点で宣材資料の作成をお願いします！">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="robots" content="noindex" />
+<link href="css\style.css" rel="stylesheet">
+</head>
+
+<body id="top">
+	<div class="wrap">
+		<header class="header">
+			<div class="header__inner"  style="text-align: center">
+				<h1 class="header__hl"><img src="images\common\logo_imas.png" alt="アイドルマスター シンデレラガールズ"></h1>
+			</div>
+		</header>
+		<main class="main">
+			<article class="main__inner">
+				<section class="section">
+					<div class="idol">
+						<h3 class="section__hl">宣伝するアイドルを<br class="spVisible">選んでください！</h3>
+						<form action="" class="form" method="GET">
+							<input type="radio" name="idol" value="" hidden checked>
+							<section class="idolSelect">
+								<h4 class="idolSelect__hl">
+									<span class="idolSelect__hl-icon"><img src="/images/common/icon_idol_princess.png" alt=""></span>
+									<span class="idolSelect__hl-princess">Cute</span>
+								</h4>
+								<ul class="idolSelect__list idolSelect__list-princess">
+									<li class="idolSelect__list-item">
+										<label class="idolSelect__list-label">
+											<input name="idol" class="idolSelect__group" value="001har" type="radio">
+											<span class="idolSelect__group-img"><img src="images\idol\chara_icon_101.png" alt="島村 卯月"></span>
+											<span class="idolSelect__group-name">島村 卯月</span>
+										</label>
+									</li>
+									<li class="idolSelect__list-item">
+										<label class="idolSelect__list-label">
+											<input name="idol" class="idolSelect__group" value="001har" type="radio">
+											<span class="idolSelect__group-img"><img src="images\idol\chara_icon_102.png" alt="島村 卯月"></span>
+											<span class="idolSelect__group-name">中野 有香</span>
+										</label>
+									</li>
+								</ul>
+							</section>
+								<p class="form__link">
+									<a href="/maker/?view=name&name=カフェイン堕天使" class="form__btn-back">戻る</a>
+									<button class="form__btn-next" type="submit">次へ</button>
+								</p>
+							</section>
+						</form>
+					</div>
+				</section>
+			</article>
+
+		</main>
+	</div>
+</body>
+</html>
---
+++ process_post.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="このページの説明文">
+  <title>このページのタイトル</title>
+  <link rel="stylesheet" href="/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+
+<?php
+  try {
+    //DB名、ユーザー名、パスワード
+    $dsn = 'mysql:dbname=imasDB;host=127.0.0.1';
+    $user = 'user';
+    $password = 'pass';
+
+    $PDO = new PDO($dsn, $user, $password); //MySQLのデータベースに接続
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //PDOのエラーレポートを表示
+
+    //input_post.phpの値を取得
+    $music_name = $_POST['music_name'];
+    $attribute = $_POST['attribute'];
+    $name = $_POST['name'];
+
+
+    $sql = "INSERT INTO imasDB (music_name, attribute, name) VALUES (:music_name, :attribute, :name)"; // INSERT文を変数に格納。:nameや:categoryはプレースホルダという、値を入れるための単なる空箱
+    $stmt = $PDO->prepare($sql); //挿入する値は空のまま、SQL実行の準備をする
+    $params = array(':music_name' => $music_name, ':attribute' => $attribute, ':name' => $name); // 挿入する値を配列に格納する
+    $stmt->execute($params); //挿入する値が入った変数をexecuteにセットしてSQLを実行
+
+    echo "<p>name: ".$music_name."</p>";
+    echo "<p>category: ".$attribute."</p>";
+    echo "<p>description: ".$name."</p>";
+    echo '<p>で登録しました。</p>'; // 登録完了のメッセージ
+  } catch (PDOException $e) {
+  exit('データベースに接続できませんでした。' . $e->getMessage());
+  }
+
+?>
+</body>
+</html>
